1d03f42da2a181322c77072692b4f1ee85b61d3d
Sambid Dahal <lifeisheaven26@gmail.com>
2022-06-23 16:08:34 +0545

    Weather C before any edit

---
+++ main.js
@@ -0,0 +1,72 @@
+const search_box = document.querySelector(".search-box");
+
+const search_button = document.querySelector(".search-button");
+
+const city_name = document.querySelector(".city");
+
+const temp_value = document.querySelector(".temp");
+
+const icon_value = document.querySelector(".icon");
+
+const description_value = document.querySelector(".description");
+
+const humidity_value = document.querySelector(".humidity");
+
+const wind_value= document.querySelector(".wind");
+
+
+
+
+let weather = {
+    apikey : "32c00f94785b9955d05d2f292fb16843",
+
+    fetchweather : function(city){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`)
+        .then(response => response.json())
+        .then(data =>{
+            this.displayWeather(data)
+        })
+       
+
+    },
+
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed } = data.wind;
+
+
+        city_name.innerText = "Weather in "+ name+ "."
+        temp_value.innerText = temp + "°C";
+        humidity_value.innerText = "Humidity:   "+   humidity +"   %";
+        wind_value.innerText = "Wind:   "+ speed +" Km/h";
+        description_value.innerText = description;
+        console.log(icon)
+        icon_value.src= "icons/"+icon+".png";
+        // `<img src="icons/${icon}.png"/>`
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+description+"/"+name+"')"
+        console.log(icon_value)
+        console.log(name, icon, description, temp, humidity,speed)
+    },
+    search: function(){
+        this.fetchweather(search_box.value);
+    }
+};
+
+
+search_button.addEventListener('click',function(){
+    weather.search();
+});
+
+search_box.addEventListener('keyup', function(event){
+    if(event.key == "Enter"){
+        weather.search(); 
+    }
+})
+
+
+
+
+
+weather.fetchweather("kathmandu");
