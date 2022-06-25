45f504901c36481f2b11a194bfa0211da55dd0d1
Sambid Dahal <lifeisheaven26@gmail.com>
2022-06-25 18:46:22 +0545

    country name added along with C to F conversion

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ const wind_value= document.querySelector(".wind");
 
 
 
+const country_value= document.querySelector(".country");
+
+
+
 
 let weather = {
     apikey : "32c00f94785b9955d05d2f292fb16843",
@@ -24,9 +28,12 @@ let weather = {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`)
         .then(response => response.json())
         .then(data =>{
+            console.log(data)
             this.displayWeather(data)
         })
-       
+       .catch(function(error){
+        alert("No Place Found")
+       })
 
     },
 
@@ -35,19 +42,39 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed } = data.wind;
+        const {country} = data.sys;
+        
 
-
-        city_name.innerText = "Weather in "+ name+ "."
-        temp_value.innerText = temp + "°C";
+    
+        city_name.innerText = "Weather in "+ name
+        temp_value.innerText = Math.floor(temp) + "°C";
         humidity_value.innerText = "Humidity:   "+   humidity +"   %";
         wind_value.innerText = "Wind:   "+ speed +" Km/h";
         description_value.innerText = description;
+        country_value.innerText = country;
+
+        let a="celsius"
+        temp_value.addEventListener('click',function(){
+            
+            
+        if(a=="celsius"){
+            temp_value.innerText = Math.floor(temp) + "°C";
+            a="farhenite"
+        }
+        else{
+            temp_value.innerHTML = Math.floor((temp*9/5)+32)+ "°F"
+            a="celsius"
+        }
+
+        })
+        
+
         console.log(icon)
         icon_value.src= "icons/"+icon+".png";
         // `<img src="icons/${icon}.png"/>`
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+description+"/"+name+"')"
         console.log(icon_value)
-        console.log(name, icon, description, temp, humidity,speed)
+        console.log(name, icon, description, temp, humidity,speed, country)
     },
     search: function(){
         this.fetchweather(search_box.value);
